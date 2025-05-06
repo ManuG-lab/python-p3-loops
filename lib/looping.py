e43905a5539074f99d4aa94dac488c8f146d87bb
Emmanuel Gitau <gmannu2055@gmail.com>
2025-05-06 10:47:56 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
+    for i in range(11):
+        print(f"{i}")
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [int * int for int in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range (1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
     pass
